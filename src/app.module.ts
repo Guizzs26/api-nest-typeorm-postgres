9e5511531cd10ebeb5bd7398d1febb7cc6dc28ef
Guilherme <guiteixeira1926@gmail.com>
2024-03-19 15:01:21 -0400

    feat: Create a connection between typeorm and postgres container. Add typerom decorators to course entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CoursesModule } from './courses/courses.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [CoursesModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
