3cfe565a4f89fe4ba0cb3ae62adfaba09e25f0c0
Guilherme <guiteixeira1926@gmail.com>
2024-03-19 13:24:15 -0400

    feat: Add depedency inversion in courses controller and improving the code

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -2,12 +2,11 @@ import {
   Controller,
   Body,
   Param,
-  Res,
   Get,
   Post,
-  Patch,
   Delete,
   HttpCode,
+  Put,
 } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 
@@ -17,42 +16,40 @@ export class CoursesController {
 
   @Get()
   listAll() {
-    return 'Listagem de Cursos';
+    return this.courseService.findAll();
   }
 
-  @Get()
-  findAllWithRes(@Res() response) {
-    return response.status(200).json({
-      message: 'Enviando uma mensagem com o decoretor Res do express',
-    });
-  }
+  // @Get()
+  // findAllWithRes(@Res() response) {
+  //   return response.status(200).json({
+  //     message: 'Enviando uma mensagem com o decoretor Res do express',
+  //   });
+  // }
 
   @Get(':id')
-  listOne(@Param('id') id: string) {
-    return `Listando o curso com o id ${id}`;
+  listOne(@Param('id') id: number) {
+    return this.courseService.findOne(+id);
   }
 
-  @Get(':id/:name')
-  listOneManyParams(@Param('id') id: string, @Param('name') name: string) {
-    return `Listando o curso com o id ${id} e nome ${name}`;
-  }
+  // @Get(':id/:name')
+  // listOneManyParams(@Param('id') id: number, @Param('name') name: string) {
+  //   return `Listando o curso com o id ${id} e nome ${name}`;
+  // }
 
   @Post()
   create(@Body() body) {
-    return body;
+    return this.courseService.create(body);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    console.log(body);
-
-    return `Update do curso com id ${id} e com os possíveis dados do body ${body}`;
+  @Put(':id')
+  update(@Param('id') id: number, @Body() body) {
+    return this.courseService.update(+id, body);
   }
 
   @HttpCode(204)
   // @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return `Deletendo o curso com o id ${id}`;
+  remove(@Param('id') id: number) {
+    return this.courseService.remove(+id);
   }
 }
