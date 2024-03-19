9bc07a156249e95acc69f9b7a45436bd2688ceb5
Guilherme <guiteixeira1926@gmail.com>
2024-03-19 12:49:22 -0400

    feat: Add courses controller with some methods

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Controller,
+  Body,
+  Param,
+  Res,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  HttpCode,
+} from '@nestjs/common';
+
+@Controller('courses')
+export class CoursesController {
+  @Get()
+  listAll() {
+    return 'Listagem de Cursos';
+  }
+
+  @Get()
+  findAllWithRes(@Res() response) {
+    return response.status(200).json({
+      message: 'Enviando uma mensagem com o decoretor Res do express',
+    });
+  }
+
+  @Get(':id')
+  listOne(@Param('id') id: string) {
+    return `Listando o curso com o id ${id}`;
+  }
+
+  @Get(':id/:name')
+  listOneManyParams(@Param('id') id: string, @Param('name') name: string) {
+    return `Listando o curso com o id ${id} e nome ${name}`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    console.log(body);
+
+    return `Update do curso com id ${id} e com os possíveis dados do body ${body}`;
+  }
+
+  @HttpCode(204)
+  // @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Deletendo o curso com o id ${id}`;
+  }
+}
