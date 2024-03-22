dad3e80c9bfa6d82474e98c3ab2f442599f89e68
Guilherme <guiteixeira1926@gmail.com>
2024-03-22 09:27:15 -0400

    refactor: Refactoring databasemodule and orm.cli.config for use environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CoursesModule } from './courses/courses.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CoursesModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CoursesModule,
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/database/orm.cli.config.ts
+++ src/database/orm.cli.config.ts
@@ -1,11 +1,24 @@
-import { DataSource } from 'typeorm';
-import { dataSourceOptions } from './database.module';
+import 'dotenv/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { CreateCoursesTable1710983440953 } from 'src/migrations/1710983440953-CreateCoursesTable';
 import { CreateTagsTable1710984626456 } from 'src/migrations/1710984626456-CreateTagsTable';
 import { CreateCoursesTagsTable1710985298566 } from 'src/migrations/1710985298566-CreateCoursesTagsTable';
 import { AddCoursesTagsColumnsToCoursesTagsTable1710986409505 } from 'src/migrations/1710986409505-AddCoursesTagsColumnsToCoursesTagsTable';
 import { AddForeignKeyToCoursesTagsTable1710986625261 } from 'src/migrations/1710986625261-AddForeignKeyToCoursesTagsTable';
 import { AddTagsIdToCoursesTagsTable1710987011470 } from 'src/migrations/1710987011470-AddTagsIdToCoursesTagsTable';
+import { Course } from 'src/courses/entities/course.entity';
+import { Tag } from 'src/courses/entities/entity.entity';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [Course, Tag],
+  synchronize: false,
+};
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
